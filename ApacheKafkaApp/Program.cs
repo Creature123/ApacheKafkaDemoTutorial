09ec5551aa1c9ffb2669802aea820ad99ee86a86
Sourav Das <sourav.besu.it.001@gmail.com>
2022-10-30 16:41:13 +0530

    Code Checking
    Code Checking

--- ApacheKafkaApp/Program.cs
+++ ApacheKafkaApp/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ApacheKafkaApp
 {
@@ -13,6 +15,15 @@ namespace ApacheKafkaApp
     {
         public static void Main(string[] args)
         {
+            string envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{envName}.json", optional: true)
+                .AddCommandLine(args).Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config).CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +32,12 @@ namespace ApacheKafkaApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    var isDevelopment = environment == Environments.Development;
+                    if(!isDevelopment)
+                    {
+                        webBuilder.UseKestrel(options => options.AddServerHeader = false);
+                    }
                 });
     }
 }
